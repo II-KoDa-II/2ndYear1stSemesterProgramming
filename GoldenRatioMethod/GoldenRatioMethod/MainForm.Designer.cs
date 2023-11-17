d5882afe22cf4d705180eac65f48abcdd929664a
Koff Daniil <119159791+II-KoDa-II@users.noreply.github.com>
2023-11-18 04:10:16 +0700

    Add files via upload

---
+++ GoldenRatioMethod/GoldenRatioMethod/MainForm.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace GoldenRatioMethod
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.рассчитатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label10 = new System.Windows.Forms.Label();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.menuStrip1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// chart1
+			// 
+			this.chart1.BackColor = System.Drawing.Color.LightGray;
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			this.chart1.Location = new System.Drawing.Point(397, 43);
+			this.chart1.Name = "chart1";
+			series1.BorderWidth = 3;
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series1.Name = "Series1";
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(600, 413);
+			this.chart1.TabIndex = 0;
+			this.chart1.Text = "chart1";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(49, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(60, 24);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "F(x) =";
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.LightGray;
+			this.panel1.Controls.Add(this.textBox7);
+			this.panel1.Controls.Add(this.label8);
+			this.panel1.Controls.Add(this.textBox3);
+			this.panel1.Controls.Add(this.textBox2);
+			this.panel1.Controls.Add(this.label4);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(12, 43);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(370, 218);
+			this.panel1.TabIndex = 2;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox7.Location = new System.Drawing.Point(116, 64);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(238, 29);
+			this.textBox7.TabIndex = 9;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label8.Location = new System.Drawing.Point(13, 64);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(96, 24);
+			this.label8.TabIndex = 8;
+			this.label8.Text = "Точность";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox3.Location = new System.Drawing.Point(20, 168);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(139, 26);
+			this.textBox3.TabIndex = 7;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox2.Location = new System.Drawing.Point(212, 168);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(139, 26);
+			this.textBox2.TabIndex = 6;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label4.Location = new System.Drawing.Point(261, 145);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(20, 20);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "B";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(61, 145);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(20, 20);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "A";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(77, 112);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(208, 24);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Укажите ограничения";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(116, 13);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(238, 29);
+			this.textBox1.TabIndex = 2;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.рассчитатьToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(1009, 33);
+			this.menuStrip1.TabIndex = 3;
+			this.menuStrip1.Text = "menuStrip1";
+			this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+			// 
+			// рассчитатьToolStripMenuItem
+			// 
+			this.рассчитатьToolStripMenuItem.BackColor = System.Drawing.SystemColors.AppWorkspace;
+			this.рассчитатьToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 14F);
+			this.рассчитатьToolStripMenuItem.Name = "рассчитатьToolStripMenuItem";
+			this.рассчитатьToolStripMenuItem.Size = new System.Drawing.Size(121, 29);
+			this.рассчитатьToolStripMenuItem.Text = "Рассчитать";
+			this.рассчитатьToolStripMenuItem.Click += new System.EventHandler(this.рассчитатьToolStripMenuItem_Click);
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.LightGray;
+			this.panel2.Controls.Add(this.label10);
+			this.panel2.Controls.Add(this.textBox6);
+			this.panel2.Controls.Add(this.textBox5);
+			this.panel2.Controls.Add(this.label7);
+			this.panel2.Controls.Add(this.label6);
+			this.panel2.Location = new System.Drawing.Point(12, 277);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(370, 179);
+			this.panel2.TabIndex = 4;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label10.Location = new System.Drawing.Point(127, 14);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(134, 26);
+			this.label10.TabIndex = 8;
+			this.label10.Text = "Результаты";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox6.Location = new System.Drawing.Point(116, 122);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(238, 29);
+			this.textBox6.TabIndex = 5;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox5.Location = new System.Drawing.Point(116, 77);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(238, 29);
+			this.textBox5.TabIndex = 4;
+			this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label7.Location = new System.Drawing.Point(8, 122);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(101, 24);
+			this.label7.TabIndex = 2;
+			this.label7.Text = "Максимум";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label6.Location = new System.Drawing.Point(15, 77);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(94, 24);
+			this.label6.TabIndex = 1;
+			this.label6.Text = "Минимум";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1009, 471);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.chart1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "MainForm";
+			this.Text = "MainForm";
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TextBox textBox7;
+    private System.Windows.Forms.Label label8;
+    private System.Windows.Forms.TextBox textBox3;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem рассчитатьToolStripMenuItem;
+    private System.Windows.Forms.Panel panel2;
+    private System.Windows.Forms.TextBox textBox6;
+    private System.Windows.Forms.TextBox textBox5;
+    private System.Windows.Forms.Label label7;
+    private System.Windows.Forms.Label label6;
+    private System.Windows.Forms.Label label10;
+  }
+}
---
+++ GoldenRatioMethod/GoldenRatioMethod/MainForm.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using org.mariuszgromada.math.mxparser;
+
+namespace GoldenRatioMethod
+{
+  public partial class MainForm : Form
+  {
+    private short MethodsKey = 4;
+    private double RestrictionStart, RestrictionEnd, Precision;
+    private double x, y;
+    string StringFunction;
+    int AccuracyForView;
+    Exceptions ex = new Exceptions();
+    public MainForm()
+    {
+      InitializeComponent();
+    }
+
+    private void SetAccuracy()
+    {
+      double Accuracy = Math.Log10(Precision) * -1.0;
+      AccuracyForView = Convert.ToInt32(Accuracy);
+      if (AccuracyForView < 0)
+      {
+        AccuracyForView = 0;
+      }
+      Console.WriteLine("Точность - " + AccuracyForView);
+    }
+
+    private void LableClear()
+    {
+      textBox5.Text = "";
+      textBox6.Text = "";
+    }
+
+    private double newtons(CalculateFunction func, DerivFunction derv)
+    {
+      double x0, xn,  xnp1;
+      x0 = RestrictionEnd;
+      xn = x0 - (func.getFunction(x0)/ derv.getDeriv(x0));
+      xnp1 = xn - (func.getFunction(xn) / derv.getDeriv(xn));
+
+      while (xn - xnp1 >= Precision)
+      {
+        xn = xnp1;
+        xnp1 = xn - (func.getFunction(xn) / derv.getDeriv(xn));
+      }
+      return xnp1;
+    }
+
+    private double GoldenRatioMethod(CalculateFunction func)
+    {
+      double Start = RestrictionStart;
+      double End = RestrictionEnd;
+      double z = (3 - Math.Sqrt(5)) / 2;
+      double x1 = Start + z * (End - Start);
+      double x2 = End - z * (End - Start);
+
+      while(Math.Abs(End - Start) > Precision)
+      {
+        if (func.getFunction(x1) <= func.getFunction(x2))
+        {
+          End = x2;
+          x2 = x1;
+          x1 = Start + End - x2;
+        }
+        else
+        {
+          Start = x1;
+          x1 = x2;
+          x2 = Start + End - x1;
+        }
+      }
+      double x = (Start + End) / 2;
+      return(x);
+    }
+
+    private double FindIntersectionPointsDichotomy(CalculateFunction func)
+    {
+      double Start = RestrictionStart;
+      double End = RestrictionEnd;
+
+      while (Math.Abs(End - Start) > Precision)
+      {
+        double MidPoint = (Start + End) / 2.0;
+        if (func.getFunction(End) * func.getFunction(MidPoint) < 0)
+        {
+          Start = MidPoint;
+        }
+        else
+        {
+          End = MidPoint;
+        }
+      }
+
+      double Accuracy = Math.Log10(Precision) * -1.0;
+      int AccuracyForView = Convert.ToInt32(Accuracy);
+      if (AccuracyForView < 0)
+      {
+        AccuracyForView = 0;
+      }
+      return Math.Round((Start + End) / 2.0);
+    }
+
+    private double CoordinateDescent(CalculateFunction func)
+    {
+      double Start = RestrictionStart;
+      double End = RestrictionEnd;
+      double x = (Start + End)/2;
+
+      while (true)
+      {
+        if (x - Precision <= Start || x + Precision >= End)
+        {
+          break;
+        }
+
+        if(func.getFunction(x) > func.getFunction(x - Precision))
+        {
+          x -= Precision;
+        }
+        else if(func.getFunction(x) > func.getFunction(x + Precision))
+        {
+          x += Precision; 
+        }
+        else
+        {
+          break;
+        }
+      }
+      Console.WriteLine("Спуск: " + x);
+      return (x);
+    }
+
+    private void FindTheMinimumGolden()
+    {
+      CalculateFunction Function = new StandartFunction(StringFunction);
+      textBox5.Text = Convert.ToString(Math.Round(GoldenRatioMethod(Function), AccuracyForView));
+    }
+
+    private void FindTheMinimumCoordinateMethod()
+    {
+      CalculateFunction Function = new StandartFunction(StringFunction);
+      textBox5.Text = Convert.ToString(Math.Round(CoordinateDescent(Function), AccuracyForView));
+    }
+
+    private void FindTheMaximumCoordinateMethod()
+    {
+      CalculateFunction Function = new MinusFunction(StringFunction);
+      textBox6.Text = Convert.ToString(Math.Round(CoordinateDescent(Function), AccuracyForView));
+    }
+
+    private void FindTheMaximumGolden()
+    {
+      CalculateFunction Function = new MinusFunction(StringFunction);
+      textBox6.Text = Convert.ToString(Math.Round(GoldenRatioMethod(Function), AccuracyForView));
+    }
+
+    private void рассчитатьToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        StringFunction = textBox1.Text;
+        Function f = new Function("f(x) = " + StringFunction);
+        if (double.IsNaN(f.calculate(1)))
+        {
+          throw new Exception();
+        }
+      }
+      catch
+      {
+        ex.showFunctionError();
+      }
+
+      try
+      {
+        RestrictionStart = Convert.ToDouble(textBox3.Text);
+        RestrictionEnd = Convert.ToDouble(textBox2.Text);
+        Precision = Convert.ToDouble(textBox7.Text);
+      }
+      catch
+      {
+        ex.showDataEntryError();
+      }
+      if (Precision <= 0)
+      {
+        ex.showPrecisionError();
+      }
+
+      CalculateFunction Function = new StandartFunction(StringFunction);
+      if (RestrictionStart < RestrictionEnd)
+      {
+        x = RestrictionStart;
+        this.chart1.Series[0].Points.Clear();
+        while (x <= RestrictionEnd)
+        {
+          y = Function.getFunction(x);
+          this.chart1.Series[0].Points.AddXY(x, y);
+          x += 1;
+        }
+      }
+      else
+      {
+        ex.showRestrictionsError();
+      }
+      LableClear();
+      SetAccuracy();
+      switch (MethodsKey)
+      {
+        case 4:
+          FindTheMinimumCoordinateMethod();
+          FindTheMaximumCoordinateMethod();
+          break;
+        default:
+          ex.showSelectedMethodError();
+          break;
+      }
+    }
+
+    private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+    {
+
+    }
+
+    private void textBox5_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+		private void label2_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
